cce34daff53c10106a17a7cab2249d44eea0f25e
hasanli-hasan <hasanih@code.edu.az>
2020-12-22 21:49:01 +0400

    5 commit

--- first-movie-app/src/components/App.js
+++ first-movie-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import axios from 'axios'
 
 class App extends React.Component {
 
@@ -13,31 +14,59 @@ class App extends React.Component {
         searchQuery: ""
     }
 
- async componentDidMount(){
-    const baseURL=" http://localhost:3002/movies";
-    const response=await fetch(baseURL);
-    const data=await response.json();
-    console.log(response)
-    console.log(data)
-    this.setState({movies:data})
-}
+    //  async componentDidMount(){
+    //     const baseURL=" http://localhost:3002/movies";
+    //     const response=await fetch(baseURL);
+    //     const data=await response.json();
+    //     console.log(response)
+    //     console.log(data)
+    //     this.setState({movies:data})
+    // }
 
-    deleteMovie = (movie) => {
+    async componentDidMount() {
+        const response = await axios.get("http://localhost:3002/movies")
+        this.setState({ movies: response.data })
+    }
+
+    // deleteMovie = (movie) => {
+    //     const newMovieList = this.state.movies.filter(
+    //         m => m.id !== movie.id
+    //     )
+
+    //     this.setState(state => ({
+    //         movies: newMovieList
+    //     }))
+    // }
+
+    //FETCH API
+    //silinen film api json-dan da silinir
+    // deleteMovie = async (movie) => {
+    //     const baseURL=`http://localhost:3002/movies/${movie.id}`;
+    //     await fetch(baseURL,{
+    //         method:"DELETE"
+    //     })
+
+    //     const newMovieList = this.state.movies.filter(
+    //         m => m.id !== movie.id
+    //     )
+    //     this.setState(state => ({
+    //         movies: newMovieList
+    //     }))
+    // }
+
+    //AXIOS API 
+    deleteMovie = async (movie) => {
+
+        axios.delete(`http://localhost:3002/movies/${movie.id}`)
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         )
-
-        //eger yuxardaki array hazir sekilde elimizde olmasa bu formada yazmaq daha mentiqlidir
-        // this.setState({
-        // movies:newMovieList
-
-        // });
-
         this.setState(state => ({
             movies: newMovieList
         }))
     }
 
+    //search
     searchMovie = (event) => {
         this.setState({ searchQuery: event.target.value })
     }
