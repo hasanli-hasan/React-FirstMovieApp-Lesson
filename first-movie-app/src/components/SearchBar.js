a1baa298d43fa7ea30d0ed37a1c33d020db7bb06
hasanli-hasan <hasanih@code.edu.az>
2020-12-22 20:54:18 +0400

    4 commit

--- first-movie-app/src/components/SearchBar.js
+++ first-movie-app/src/components/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <div className="form-row m-5">
+                <div className="form-row mb-2">
                     <div className="col-12">
 
                         <input onChange={this.props.SearchMovieProp} 
